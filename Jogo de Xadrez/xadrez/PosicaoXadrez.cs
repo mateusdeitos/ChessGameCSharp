e5db195afe88e40e1f14841ddd64013ebd2fb86b
Mateus Deitos <mate.deitos@hotmail.com>
2019-10-23 20:32:31 -0300

    Criando a classe PosicaoXadrez

---
+++ Jogo de Xadrez/xadrez/PosicaoXadrez.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using tabuleiro;
+
+namespace xadrez
+{
+    class PosicaoXadrez
+    {
+        public char coluna { get; set; }
+        public int linha { get; set; }
+
+        public PosicaoXadrez(char coluna, int linha)
+        {
+            this.coluna = coluna;
+            this.linha = linha;
+        }
+
+        public Posicao toPosicao()
+        {
+            return new Posicao(8 - linha, coluna - 'a'); //a-a = 0, b-a = 1, c-a = 2, ...
+        }
+
+        public override string ToString()
+        {
+            return "" + coluna + linha;
+        }
+    }
+}
